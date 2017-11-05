a090bc0f910acfc627a8938e0c3bbf7604e6a808
jiangxingqi <jxq0816@126.com>
2017-11-05 12:28:42 +0800

    css 样式更新

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
         width: 90%;height: 0;margin: 20px auto 0 auto;
         border-style: solid;border-color: #458994;border-width: 1px 0 0 0
     }
-    .af {width: 90%;margin: 0 auto 0 auto}
+    .af {width: 90%;margin: 0 auto 0 auto;}
     .af img{float: right;width: 33px;height:22px;margin: 2px 5px 0 0}
-    .af span{float:left;font-family: sans-serif,宋体;margin: 12px 0 0 5px}
+    .af span{ width:100%;display:inline-block;font-family: sans-serif,宋体;margin: 12px 0 0 5px;text-align: center}
 </style>
 <div class="h1"></div>
 <div class="af">
