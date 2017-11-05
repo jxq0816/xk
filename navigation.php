d84f575fedc4870274a43d5faa6a0722f461eca4
jiangxingqi <jxq0816@126.com>
2017-11-06 00:37:10 +0800

    导航栏

--- navigation.php
+++ navigation.php
@@ -3,20 +3,41 @@
     <head>
         <title>navigation</title>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="/xk/css/xk.css?random=1"/>
+        <link rel="stylesheet" type="text/css" href="/xk/css/xk.css"/>
         <?php
+            $isLogin = TRUE;
+            $lb="同学";
             $name="jiangxingqi";
             $choice=1;
         ?>
     </head>
     <body>
         <div class="nav">
-            <a class="right" href="#">退出</a>
-            <span class="right"><?php echo $name.",您好"?></span>
-            <a class='left <?php echo $choice==1 ? "current" : ""?>' href="teacher_p.php">浏览教师信息</a>
-            <a class='left <?php echo $choice==2 ? "current" : ""?>' href="course_p.php">添加课程</a>
-            <a class='left <?php echo $choice==3 ? "current" : ""?>' href="opencourse_p.php">维护开课信息</a>
+            <?php 
+                if($isLogin==false){ 
+            ?>
+                <a class='right <?php echo $choice==2 ? "current" : ""?>' href="#">注册</a>
+                <a class='right <?php echo $choice==1 ? "current" : ""?>' href="#">登录</a>
+            <?php
+                }else{
+             ?>
+            
+            <a class='right' href="logoff.php">退出登录</a>
+            <a class='right' href="enter.php">进入系统</a>
+            <span class="right"><?php echo $name.$lb.",您好"?></span>
+            
+            
+           
+           
+           
+            <a class='left <?php echo $choice==1 ? "current" : ""?>' href="teacher_p.php">浏览课程信息</a>
+            <a class='left <?php echo $choice==2 ? "current" : ""?>' href="course_p.php">选课</a>
+            <a class='left <?php echo $choice==3 ? "current" : ""?>' href="opencourse_p.php">查看成绩</a>
             <div style="clear:both"></div>
+            
+            <?php 
+                } 
+            ?>
         </div>
     </body>
 </html>
---
+++ student/navigation_student.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>navigation</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="/xk/css/xk.css"/>
+        <?php
+            $name="jiangxingqi";
+            $choice=1;
+        ?>
+    </head>
+    <body>
+        <div class="nav">
+           
+            <a class='right' href="logoff.php">退出</a>
+            <span class="right"><?php echo $name.",您好!"?></span>
+           
+           
+            <a class='left <?php echo $choice==1 ? "current" : ""?>' href="course.php">浏览课程信息</a>
+            <a class='left <?php echo $choice==2 ? "current" : ""?>' href="elective.php">选课</a>
+            <a class='left <?php echo $choice==3 ? "current" : ""?>' href="score_view.php">查看成绩</a>
+            
+            <div style="clear:both"></div>
+            
+        </div>
+    </body>
+</html>
